b96b583529e34f459d654051ab9cd884f23089d0
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-03-24 10:26:36 +0000

    run tickets migration in the database

--- CustomerCare/database/migrations/2025_03_24_093409_create_tickets_table.php
+++ CustomerCare/database/migrations/2025_03_24_093409_create_tickets_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('status');
+            $table->string('subject');
+            $table->text('description');
             $table->timestamps();
         });
     }
