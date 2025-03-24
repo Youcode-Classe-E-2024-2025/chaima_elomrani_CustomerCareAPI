a8f7a681aca02384a4e5f1c6b07e11addaf6fd88
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-03-24 15:37:41 +0000

    added the route

--- CustomerCare/routes/api.php
+++ CustomerCare/routes/api.php
@@ -1,15 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TicketsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
+
+Route::post('/register', [AuthController::class, 'register']);
+
+
+//CRUD methods
 Route::get('/showTickets', [TicketsController::class, 'index']);
 Route::post('/storeTickets', [TicketsController::class, 'store']);
 Route::get('/show/{id}', [TicketsController::class, 'show']);
 Route::put('/update/{id}', [TicketsController::class, 'update']);
 Route::delete('/destroy/{id}', [TicketsController::class, 'destroy']);
+
