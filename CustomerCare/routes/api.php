d1bda92fbd84e757dcb9568fd8452ae7ad2b8861
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-03-25 13:51:43 +0000

    debogage for testing

--- CustomerCare/routes/api.php
+++ CustomerCare/routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'login']);
-
+Route::get('/logout', [AuthController::class, 'logout']);
 
 //CRUD methods
 Route::get('/showTickets', [TicketsController::class, 'index']);
@@ -17,4 +17,6 @@ Route::post('/storeTickets', [TicketsController::class, 'store']);
 Route::get('/show/{id}', [TicketsController::class, 'show']);
 Route::put('/update/{id}', [TicketsController::class, 'update']);
 Route::delete('/destroy/{id}', [TicketsController::class, 'destroy']);
+
+
 
