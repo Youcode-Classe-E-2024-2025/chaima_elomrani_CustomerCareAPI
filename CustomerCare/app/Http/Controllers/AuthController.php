f10207e45770d5ec01727f42a352aa700bb2e8b9
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-03-27 13:54:51 +0000

    updated the lougout method to verify if the user is loged in or not

--- CustomerCare/app/Http/Controllers/AuthController.php
+++ CustomerCare/app/Http/Controllers/AuthController.php
@@ -11,36 +11,47 @@ use App\Services\AuthService;
 
 class AuthController extends Controller
 {
-  private $authService ;
+    private $authService;
 
-  public function __construct(AuthService $authService)
-  {
-    $this->authService = $authService;
-  }
-
-  public function register(Request $request){
-    return $this->authService->register($request);
-  }
-
-
-  public function login(Request $request){
-
-    $request->only(['email', 'password']);
-
-    if($this->authService->login($request)){
-        return redirect()->route('dashboard');
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
     }
-    return redirect()->back()->withErrors(['email, password' => 'Invalid credentials']);
-  }
+
+    public function register(Request $request)
+    {
+        return $this->authService->register($request);
+    }
 
 
- public function logout(Request $request){
+    public function login(Request $request)
+    {
 
-    $this->authService->logout($request);
+        $request->only(['email', 'password']);
 
-    return redirect()->route('login');
- }
-  
-} 
+        if ($this->authService->login($request)) {
+            return response()->json(['status' => 'success', 'message' => 'Logged in successfully']);
+        }
+        return redirect()->back()->withErrors(['email, password' => 'Invalid credentials']);
+    }
+
+
+    public function logout(Request $request)
+    {
+        if ($request->user()) {
+            $request->user()->currentAccessToken()->delete();
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Logged out successfully',
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'You are not logged in',
+            ], 401);
+        }
+    }
+
+}
 
 
