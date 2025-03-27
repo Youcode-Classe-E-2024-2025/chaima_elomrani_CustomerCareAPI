6aad8d5c3d773a64d3d7e139f4a7fef4529bb9c9
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-03-27 13:36:18 +0000

    added the register method in the controller

--- CustomerCare/app/Http/Controllers/AuthController.php
+++ CustomerCare/app/Http/Controllers/AuthController.php
@@ -6,8 +6,19 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
+use App\Services\AuthService;
+
 
 class AuthController extends Controller
 {
-  
+  private $authService ;
+
+  public function __construct(AuthService $authService)
+  {
+    $this->authService = $authService;
+  }
+
+  public function register(Request $request){
+    return $this->authService->register($request);
+  }
 } 
