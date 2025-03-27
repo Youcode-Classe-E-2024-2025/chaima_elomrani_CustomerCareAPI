393f3c6f86afa37d2ed1dfd773beea8a6b468ea2
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-03-27 13:56:52 +0000

    added responces routes

--- CustomerCare/routes/api.php
+++ CustomerCare/routes/api.php
@@ -11,12 +11,17 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout']);
 
-//CRUD methods
+// tickets CRUD 
 Route::get('/showTickets', [TicketsController::class, 'index']);
 Route::post('/storeTickets', [TicketsController::class, 'store']);
 Route::get('/show/{id}', [TicketsController::class, 'show']);
 Route::put('/update/{id}', [TicketsController::class, 'update']);
 Route::delete('/destroy/{id}', [TicketsController::class, 'destroy']);
 
-
+// responses CRUD 
+Route::get('/showServices', [TicketsController::class, 'index']);
+Route::post('/storeServices', [TicketsController::class, 'store']);
+Route::get('/show/{id}', [TicketsController::class, 'show']);
+Route::put('/update/{id}', [TicketsController::class, 'update']);
+Route::delete('/destroy/{id}', [TicketsController::class, 'destroy']);
 
